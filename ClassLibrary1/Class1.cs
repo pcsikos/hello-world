8bbf591dc645129f98b2eb1b95ea9ea6b849e802
Peter Csikos <pcsikos@gmail.com>
2015-10-22 19:32:37 +0200

    Classlibrary

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static bool And(bool a, bool b)
+        {
+            if (a && b)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod]
+        public void And_BothTrueGiven_ShouldReturnTrue()
+        {
+            var result = Class1.And(true, true);
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void And_AnyFalseGiven_ShouldReturnFalse()
+        {
+            var result = Class1.And(true, false);
+            Assert.AreEqual(result, false);
+        }
+    }
+}
