c256303bc4da8c902fe5b9067f664a1fba7b7e82
Peter Csikos <pcsikos@gmail.com>
2015-10-22 20:02:07 +0200

    CI

--- ClassLibrary1Tests/Class1Tests.cs
+++ ClassLibrary1Tests/Class1Tests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ClassLibrary1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary1.Tests
 {
